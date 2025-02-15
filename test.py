fd546f04d0237b9fa540dff9003436e826108f19
saumyadileesh <72030306+saumyadileesh@users.noreply.github.com>
2025-02-15 14:36:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # This is a simple Python script
 
 # Print a greeting message
-print("Hello, this is not a sample Python script!")
+print("Hello, this is not a sample Python script!, I like Python")
 
 # You can add more print statements to show other messages
 print("Python is not great for automation and scripting.")
